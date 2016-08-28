537dc0f413980b0b03f6a4783406d78634e69775
Kimchip <kberichon@gmail.com>
2016-08-28 15:41:36 +0800

    Initial Commit

---
+++ saveTheDogs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  saveTheDogs
+//
+//  Created by Kim Berichon on 28/8/16.
+//  Copyright © 2016 newDay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
